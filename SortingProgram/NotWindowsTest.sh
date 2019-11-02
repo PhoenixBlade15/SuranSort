43c0158c57de32f11b7a53b238af95ad2509362b
PhoenixBlade15 <43272926+PhoenixBlade15@users.noreply.github.com>
2019-11-01 23:38:30 -0400

    Update NotWindowsTest.sh

--- SortingProgram/NotWindowsTest.sh
+++ SortingProgram/NotWindowsTest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export DESC=False
-python3 Main.py $DESC
+python3 Main.py False
 diff Sorted.txt Expected.txt
 ascOut=$?
 
 export DESC=True
-python3 Main.py $DESC
+python3 Main.py True
 diff SortedR.txt ExpectedR.txt
 descOut=$?
 
